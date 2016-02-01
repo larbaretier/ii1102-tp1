2fba3c1dcdbafef02c59f28b72eb90af645feecc
larbaretier <louis.arbaretier@gmail.com>
2016-02-01 12:18:19 +0100

    Update Pave.java

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
-
-
 public class Pave {
-    public static void main(String ... args) {
-
-	//1. Demander chaque longueur grâce au Scanner
-	Scanner scan = new Scanner(System.in);
 	
-    }
+	public static void main(String [] arge){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("hello");
+		System.out.println("hauteur");
+		long haut= scan.nextLong();
+		if (haut<0){
+			System.out.println("negatif : rerentrez la valeur ");
+			haut= scan.nextLong();
+		}
+		System.out.println("longueur");
+		long longu=scan.nextLong();
+		if (longu<0){
+			System.out.println("negatif : rerentrez la valeur ");
+			longu=scan.nextLong();
+		}
+		System.out.println("largeur");
+		long prof=scan.nextLong();
+		if (prof<0){
+			System.out.println("negatif : rerentrez la valeur ");
+			prof=scan.nextLong();
+		}
+		
+		System.out.println("volume :"+(haut*longu*prof));
+		
+	}
 }
